1ec0b10aeaecdfc591d539a580fca1a4722e1b39
Ze-Zheng Wu <zzwu@zju.edu.cn>
2023-11-09 11:07:51 +0800

    docs: add doc comments for `Position` and `Point`

--- src/bindings/position.ts
+++ src/bindings/position.ts
@@ -16,11 +16,42 @@ export interface ZXingPosition {
   bottomRight: ZXingPoint;
 }
 
+/**
+ * X, Y coordinates to describe a point.
+ *
+ * @property x X coordinate.
+ * @property y Y coordinate.
+ *
+ * @see {@link Position | `Position`}
+ */
 export interface Point extends ZXingPoint {}
 
+/**
+ * Position of the decoded barcode.
+ */
 export interface Position {
+  /**
+   * Top-left point of the decoded barcode.
+   *
+   * @see {@link Point | `Point`}
+   */
   topLeft: Point;
+  /**
+   * Top-right point of the decoded barcode.
+   *
+   * @see {@link Point | `Point`}
+   */
   topRight: Point;
+  /**
+   * Bottom-left point of the decoded barcode.
+   *
+   * @see {@link Point | `Point`}
+   */
   bottomLeft: Point;
+  /**
+   * Bottom-right point of the decoded barcode.
+   *
+   * @see {@link Point | `Point`}
+   */
   bottomRight: Point;
 }
