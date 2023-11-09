99d2fec385ea9532f8998cfe92ca793fcfadbac5
Ze-Zheng Wu <zzwu@zju.edu.cn>
2023-11-09 10:56:19 +0800

    docs: add doc comments for `WriteResult`

--- src/bindings/writeResult.ts
+++ src/bindings/writeResult.ts
@@ -3,12 +3,24 @@
  */
 export interface ZXingWriteResult {
   image: Uint8Array;
+  /**
+   * Encoding error.
+   * If there's no error, this will be an empty string `""`.
+   *
+   * @see {@link WriteResult.error | `WriteResult.error`}
+   */
   error: string;
   delete: () => void;
 }
 
 export interface WriteResult
   extends Omit<ZXingWriteResult, "image" | "delete"> {
+  /**
+   * The encoded barcode as an image blob.
+   * If some error happens, this will be `null`.
+   *
+   * @see {@link WriteResult.error | `WriteResult.error`}
+   */
   image: Blob | null;
 }
 
