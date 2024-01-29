02f0386e51941772db006b392a3b246d7e3273ac
Ze-Zheng Wu <zezhengwu@proton.me>
2024-01-29 19:47:55 +0800

    chore: bump zxing-cpp to b3fe574 and other chores

---
+++ .changeset/friendly-walls-push.md
@@ -0,0 +1,5 @@
+---
+"zxing-wasm": patch
+---
+
+Bump zxing-cpp to b3fe574
--- src/bindings/readerOptions.ts
+++ src/bindings/readerOptions.ts
@@ -84,18 +84,21 @@ export interface ZXingReaderOptions {
    * If `true`, the Code-39 reader will try to read extended mode.
    *
    * @defaultValue `false`
+   * @deprecated upstream
    */
   tryCode39ExtendedMode: boolean;
   /**
    * Assume Code-39 codes employ a check digit and validate it.
    *
    * @defaultValue `false`
+   * @deprecated upstream
    */
   validateCode39CheckSum: boolean;
   /**
    * Assume ITF codes employ a GS1 check digit and validate it.
    *
    * @defaultValue `false`
+   * @deprecated upstream
    */
   validateITFCheckSum: boolean;
   /**
