2723a250863210184c3cdc448786674b13e562d9
Ze-Zheng Wu <zezhengwu@proton.me>
2024-10-15 01:48:01 +0800

    chore: reformat snapshots

--- tests/blackbox.test.ts
+++ tests/blackbox.test.ts
@@ -1,7 +1,14 @@
 import { readFile } from "node:fs/promises";
 import { basename, extname, relative, resolve } from "node:path";
 import { glob } from "tinyglobby";
-import { beforeAll, describe, expect, test } from "vitest";
+import {
+  afterAll,
+  beforeAll,
+  describe,
+  expect,
+  onTestFinished,
+  test,
+} from "vitest";
 import {
   getZXingModule,
   readBarcodesFromImageFile,
@@ -23,14 +30,14 @@ type _Summary = {
   [type in Type]?: {
     [rotation in number]?: {
       failures: number;
-      misreads: {
+      misreads?: {
         total: number;
         images: {
           path: string;
           description: string;
         }[];
       };
-      undetected: {
+      undetected?: {
         total: number;
         images: string[];
       };
@@ -40,6 +47,8 @@ type _Summary = {
 
 interface Summary extends _Summary {
   total: number;
+  passAll: number;
+  passSome: number;
 }
 
 type Entries<T> = {
@@ -76,8 +85,12 @@ for (const {
     ).sort((a, b) => a.localeCompare(b, undefined, { numeric: true }));
     const summary: Summary = {
       total: imagePaths.length,
+      passAll: 0,
+      passSome: 0,
     };
     for (const imagePath of imagePaths) {
+      let passAll = true;
+      let passSome = false;
       const imageName = basename(imagePath, extname(imagePath));
       const shortImagePath = relative(PATH_PREFIX, imagePath);
       const [expectedResult, expectedText, expectedBinary] = await Promise.all([
@@ -86,6 +99,14 @@ for (const {
         parseExpectedBinary(imagePath),
       ]);
       describe(`${directory} ${imageName}`, async () => {
+        afterAll(() => {
+          if (passAll) {
+            ++summary.passAll;
+          }
+          if (passSome) {
+            ++summary.passSome;
+          }
+        });
         for (const type of types) {
           summary[type] ??= {};
           for (const rotation of type === "pure" ? [0] : rotations) {
@@ -101,6 +122,11 @@ for (const {
               },
             };
             test(`${directory} ${imageName} ${type} ${rotation}`, async () => {
+              let passCurrent = true;
+              onTestFinished(() => {
+                passAll &&= passCurrent;
+                passSome ||= passCurrent;
+              });
               const imageBlob = new Blob([
                 await getRotatedImage(imagePath, rotation),
               ]);
@@ -123,20 +149,22 @@ for (const {
 
               // Undetected
               if (barcode === undefined || !barcode.isValid) {
-                summary[type]![rotation]!.undetected.images.push(
+                summary[type]![rotation]!.undetected!.images.push(
                   shortImagePath,
                 );
-                summary[type]![rotation]!.undetected.total += 1;
+                summary[type]![rotation]!.undetected!.total += 1;
+                passCurrent = false;
                 return;
               }
 
               // Format mismatch
               if (barcode.format !== barcodeFormat) {
-                summary[type]![rotation]!.misreads.images.push({
+                summary[type]![rotation]!.misreads!.images.push({
                   path: shortImagePath,
                   description: `[Format mismatch]: expected '${barcodeFormat}', but got '${barcode.format}'`,
                 });
-                summary[type]![rotation]!.misreads.total += 1;
+                summary[type]![rotation]!.misreads!.total += 1;
+                passCurrent = false;
               }
 
               // .result.txt
@@ -156,33 +184,36 @@ for (const {
                   }
                 }
                 if (misread) {
-                  summary[type]![rotation]!.misreads.images.push({
+                  summary[type]![rotation]!.misreads!.images.push({
                     path: shortImagePath,
                     description,
                   });
-                  summary[type]![rotation]!.misreads.total += 1;
+                  summary[type]![rotation]!.misreads!.total += 1;
+                  passCurrent = false;
                 }
               }
 
               // .txt
               if (expectedText) {
                 if (barcode.text !== expectedText) {
-                  summary[type]![rotation]!.misreads.images.push({
+                  summary[type]![rotation]!.misreads!.images.push({
                     path: shortImagePath,
                     description: `[Text content mismatch]: expected '${expectedText}', but got '${barcode.text}'`,
                   });
-                  summary[type]![rotation]!.misreads.total += 1;
+                  summary[type]![rotation]!.misreads!.total += 1;
+                  passCurrent = false;
                 }
               }
 
               // .bin
               if (expectedBinary) {
                 if (!expectedBinary.equals(Buffer.from(barcode.bytes))) {
-                  summary[type]![rotation]!.misreads.images.push({
+                  summary[type]![rotation]!.misreads!.images.push({
                     path: shortImagePath,
                     description: "[Binary content mismatch]",
                   });
-                  summary[type]![rotation]!.misreads.total += 1;
+                  summary[type]![rotation]!.misreads!.total += 1;
+                  passCurrent = false;
                 }
               }
             });
@@ -190,14 +221,27 @@ for (const {
         }
       });
     }
-
     test.sequential(`${directory} summary`, () => {
       for (const type of types) {
-        for (const _summary of Object.values(summary[type]!)) {
+        for (const [rotation, _summary] of Object.entries(summary[type]!)) {
           _summary!.failures = new Set([
-            ..._summary!.misreads.images,
-            ..._summary!.undetected.images,
+            ..._summary!.misreads!.images,
+            ..._summary!.undetected!.images,
           ]).size;
+          if (_summary!.misreads!.total === 0) {
+            // biome-ignore lint/performance/noDelete: remove misreads if there is no misread
+            delete _summary!.misreads;
+          }
+          if (_summary!.undetected!.total === 0) {
+            // biome-ignore lint/performance/noDelete: remove undetected if there is no undetected
+            delete _summary!.undetected;
+          }
+          if (_summary!.failures === 0) {
+            delete summary[type]![rotation as unknown as number];
+          }
+        }
+        if (Object.keys(summary[type]!).length === 0) {
+          delete summary[type];
         }
       }
       expect(`${JSON.stringify(summary, null, 2)}\n`).toMatchFileSnapshot(
